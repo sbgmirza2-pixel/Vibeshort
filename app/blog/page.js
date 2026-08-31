641a9af9ed25cb3deca43b5d6b788d6663da37fe
sbgmirza@-pixel <sbgmirza2@gmail.com>
2026-08-31 22:43:35 +0500

    fix

--- app/blog/page.js
+++ app/blog/page.js
@@ -23,7 +23,7 @@ export default function BlogListingPage() {
             </p>
           </div>
 
-          {/* Horizontal Box Grid */}
+          {/* Horizontal. Box Grid */}
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
             {blogsData.map((blog) => (
               <Link 
